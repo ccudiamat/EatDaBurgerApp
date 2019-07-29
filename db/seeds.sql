0c0f4973037608f9ebc75029ec4ff7c3e2565240
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-07-28 21:18:28 -0700

    updated routes and added buttons to the index.handlebars file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("Vegan Burger");
-INSERT INTO burgers (burger_name) VALUES ("BBQ Swiss Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Swiss Burger", FALSE);
