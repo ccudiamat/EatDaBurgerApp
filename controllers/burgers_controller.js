923e763f622d94d9fb52af7caf4fccd671253169
Christopher Cudiamat <ccudiamat93@gmail.com>
2019-07-28 23:28:01 -0700

    functional app, need to update css to be more presentable

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,16 +2,22 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-router.get("/", function(req,res) {
-    burger.all(function(burger_data){
+router.get("/", function (req, res) {
+    burger.all(function (burger_data) {
         console.log(burger_data);
-        res.render("index", {burger_data});
+        res.render("index", { burger_data });
     })
-})
+});
 
-router.post("/burgers/update", function(req,res){
-    burger.update(req.body.burger_id, function(result){
+router.post("/burgers/update", function (req, res) {
+    burger.update(req.body.burger_id, function (result) {
         console.log(result);
+        res.redirect("/");
+    })
+});
+
+router.post("/burgers/create", function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
         res.redirect("/");
     })
 });
--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,18 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function(cb){
-        orm.all("burgers", function(res){
+    all: function (cb) {
+        orm.all("burgers", function (res) {
             cb(res);
         })
     },
 
-    update: function(id,cb){
+    update: function (id, cb) {
         orm.update("burgers", id, cb);
+    },
+
+    create: function (name, cb) {
+        orm.create("burgers", name, cb);
     }
 }
 
